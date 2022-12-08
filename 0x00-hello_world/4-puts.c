b7d21781ae88b87a6575baea393011592f503f1b
zekariyas7 <zekariyas7721@gmail.com>
2022-12-08 13:57:13 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <studio.h>
+
 /**
- * main - main block
- * return: 0
+ * main - print a given phrase
+ * return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	put(*\"Programming is like building a multilingual puzzle");
-	return (0)
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
 }		
